3157bb03aa71dffe6bea51c3e74cc2b15029d417
Deepika Ravichandran <deepika@myunisoft.com>
2026-02-07 17:14:46 +0530

    Added expense shown in list

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -4,39 +4,24 @@ import '../models/expense.dart';
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  // Add expense
   Future<void> addExpense(Expense expense) async {
     await _db.collection('expenses').doc(expense.id).set(expense.toMap());
   }
 
-  // Get expenses for a user
   Stream<List<Expense>> getExpenses(String userId) {
     return _db
         .collection('expenses')
         .where('userId', isEqualTo: userId)
-        .orderBy('date', descending: true)
         .snapshots()
-        .map((snapshot) => snapshot.docs
-            .map((doc) => Expense.fromMap(doc.data()))
-            .toList());
+        .map((snapshot) {
+      final expenses =
+          snapshot.docs.map((doc) => Expense.fromMap(doc.data())).toList();
+      expenses.sort((a, b) => b.date.compareTo(a.date));
+      return expenses;
+    });
   }
 
-  // Delete expense
   Future<void> deleteExpense(String expenseId) async {
     await _db.collection('expenses').doc(expenseId).delete();
-  }
-
-  // Get total expenses for a user
-  Future<double> getTotalExpenses(String userId) async {
-    final snapshot = await _db
-        .collection('expenses')
-        .where('userId', isEqualTo: userId)
-        .get();
-
-    double total = 0;
-    for (var doc in snapshot.docs) {
-      total += (doc.data()['amount'] ?? 0).toDouble();
-    }
-    return total;
   }
 }
